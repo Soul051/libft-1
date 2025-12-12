b0c35872dc1a11b0a8e2f9d0dafde777793d6327
Soul <125883891+Soul051@users.noreply.github.com>
2025-12-12 22:47:43 +0100

    Add files via upload

--- ft_strtrim.c
+++ ft_strtrim.c
@@ -6,35 +6,44 @@
 /*   By: galorenz <galorenz@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/12/09 19:44:46 by galorenz          #+#    #+#             */
-/*   Updated: 2025/12/12 19:00:55 by galorenz         ###   ########.fr       */
+/*   Updated: 2025/12/12 22:29:32 by galorenz         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-char *ft_strtrim(char const *s1, char const *set)
+char	*ft_strtrim(char const *s1, char const *set)
 {
-    char *str;
-    size_t inizio;
-    size_t fine;
-    size_t s1_lung;
-    size_t cont;
-    size_t lung;
-    
-    s1_lung = ft_strlen(s1);
-    inizio = 0;
+	char *str;
+	size_t inizio;
+	size_t fine;
+	size_t lung;
+	
+	inizio = 0;
+	fine = ft_strlen(s1);
 
-    if(!s1 || !set)
-        return(NULL);
+	if(!s1 || !set)
+		return(NULL);
+	while(s1[inizio] && ft_strchr(set, s1[inizio]))
+		inizio++;
+	while(fine > inizio && ft_strrchr(set, s1[fine - 1]))
+		fine--;
+	lung = fine - inizio;
+	str = (char *)malloc(lung +1);
+	if (str == NULL)
+		return (NULL);
+	ft_strlcpy(str, s1 + inizio, lung + 1);
+	return(str);
+}
 
-
-
-    
-    str = (char *)malloc(lung +1);
-    if ( str == NULL)
-        return (NULL);
-
-
-    ft_strlcpy(str,);
-    return(str);
+int main()
+{
+	char *parola = "omsomethingso";
+	char *parola2 = "om";
+	
+	parola = ft_strtrim(parola, parola2);
+	
+	printf("Originale: %s\n", parola);
+	free(parola);
+	
 }
